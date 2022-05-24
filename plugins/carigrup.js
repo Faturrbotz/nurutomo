65c89f5d12e71eea6a407fd1c34c331d0a0e5ad6
Faturrbotz <91567895+Faturrbotz@users.noreply.github.com>
2022-05-25 02:57:47 +0700

    Update carigrup.js

--- plugins/carigrup.js
+++ plugins/carigrup.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, text }) => {
     if (!text) throw 'Cari apa?'
     let res = await fetch(global.API('xteam', '/search/grupwa', {
         q: text
-    }, 'APIKEY'))
+    }, 'lolkey'))
     if (res.status !== 200) throw await res.text()
     let json = await res.json()
     if (!json.status) throw json
